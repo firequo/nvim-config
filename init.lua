d4d97fa0281b81d93f54ad0815eabcd81d585f7e
firequo <62629670+firequo@users.noreply.github.com>
2025-03-28 22:52:34 -0400

    add lua back to treesitter

--- init.lua
+++ init.lua
@@ -92,7 +92,7 @@ require("lazy").setup(
 vim.defer_fn(function()
     require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'java' },
+        ensure_installed = { 'java', 'lua' },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = false,
